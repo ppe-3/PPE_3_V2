4f659ebfc6644ed2b7d295d012dc7d2ce712d645
LALA <LC043266>
2018-04-05 16:56:28 +0200

    general

--- ajout.php
+++ ajout.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'src/init.php';
-include_once ROOT.'/app/controllers/ligneDeFraisController.php';
+include_once ROOT.'/app/controllers/ajoutLigneDeFraisController.php';
 include_once ROOT.'/app/templates/menu.php';
 ?>
 <header class="masthead">
---
+++ app/controllers/ajoutLigneDeFraisControlleur.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'src/init.php';
+include_once ROOT.'/app/templates/menu.php';
+include_once ROOT.'/app/entities/dao/ligneDeFraisDAO.php';
+
+$datetrajet 	= isset($_POST['datetrajet']) ? $_POST['datetrajet'] : '';
+$trajet 		= isset($_POST['trajet']) ? $_POST['trajet'] : '';
+$km             = isset($_POST['km']) ? $_POST['km'] : '';
+$ct 			= isset($_POST['ct']) ? $_POST['ct'] : '';
+$cp 			= isset($_POST['cp']) ? $_POST['cp'] : '';
+$cr 			= isset($_POST['cr']) ? $_POST['cr'] : '';
+$ch             = isset($_POST['ch']) ? $_POST['ch'] : '';
+$motif          = isset($_POST['motif']) ? $_POST['motif'] : '';
+$submit 		= isset($_POST['submit']) ? $_POST['submit'] : '';
